37ce35f2077910472466552e0cb7c435a95f673e
altaj1 <altaj1019@gmail.com>
2024-08-16 01:29:25 +0600

    card completed

--- src/components/Pages/Sopping/Card.jsx
+++ src/components/Pages/Sopping/Card.jsx
@@ -1,39 +1,55 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-const Card = ({product}) => {
-    console.log(product)
-    const {coverImage, currentPrice, name, price, 
-        _id, 
-        discount,BrandName
-
-    } = product
+const Card = ({ product }) => {
+  console.log(product);
+  const {
+    coverImage,
+    currentPrice,
+    name,
+    price,
+    _id,
+    discount,
+    BrandName,
+    ratings,
+  } = product;
   return (
     <div className="relative hover:z-50 card hover:shadow-xl p-5  duration-100 transform hover:scale-y-105 ">
-  <figure className="h-60">
-    <img
-      className="h-60 w-full rounded-b-xl"
-      src={coverImage}
-      alt="product"
-    />
-  </figure>
-  <div className="flex  gap-1 mt-1">
-    <p className="bg-[#FFD9D8] text-xs text-center py-1 px-1 rounded-sm">BigSave</p>
-    <p className="bg-[#FFE5C1] text-xs py-1 rounded-sm px-1">Brand</p>
-    <p className="text-sm ">{BrandName}</p>
-  </div>
-  <div className="">
-    <h2 className="font-semibold">
-      {name}
-    </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <figure className="h-60">
+        <img
+          className="h-60 w-full rounded-b-xl"
+          src={coverImage}
+          alt="product"
+        />
+      </figure>
+      <div className="flex  gap-1 mt-1">
+        <p className="bg-[#FFD9D8] text-xs text-center py-1 px-1 rounded-sm">
+          BigSave
+        </p>
+        <p className="bg-[#FFE5C1] text-xs py-1 rounded-sm px-1">Brand</p>
+        <p className="text-sm ">{BrandName}</p>
+      </div>
+      <div className="">
+        <h2 className="font-semibold">{name}</h2>
+        <div className="flex justify-between items-center">
+          {" "}
+         
+          <p>
+            <span className="text-xl font-medium">BDT{currentPrice}</span>{" "}
+            <span className="opacity-50 text-sm">BDT{price}</span>
+          </p>
+          <p className="text-sm">Ratings {ratings ? ratings : "4.5"}</p>
+        </div>
+        <div className="card-actions items-center justify-between mt-1">
+          <div className=" rounded-r-xl px-2 badge-outline bg-red-600 text-white font-medium">
+            {discount}% off
+          </div>
+          <div className="badge badge-outline bg-[#8dbe3f] hover:bg-[#5B8021] hover:text-white font-medium">
+            See preview
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
   );
 };
 

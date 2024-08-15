976dd3588a71db5939f1e075865c4e523db83851
altaj1 <altaj1019@gmail.com>
2024-08-15 11:05:33 +0600

    checked prices brand name item value done

--- src/components/Pages/Sopping/Sopping.jsx
+++ src/components/Pages/Sopping/Sopping.jsx
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
-import useAxiosCommon from '../../../hooks/useAxiosCommon';
-import { priductCategories } from '../Admin/utilits/utlits';
+import React, { useState } from "react";
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
+import { priceRange, priductCategories } from "../Admin/utilits/utlits";
 
 const Sopping = () => {
-    const axiosCommon = useAxiosCommon();
+  const axiosCommon = useAxiosCommon();
   const [checkedCategory, setCheckedCategory] = useState(null);
-  const [productBrand, SetProductBrand] = useState('');
+  const [selectedPrice, setSelectedPrice] = useState(null); 
+  const [productBrand, SetProductBrandName] = useState("");
   const [categoresItem, setCategoresItem] = useState([]);
-
-console.log(productBrand)
+  const [item, setItem] = useState("");
+  const [price, setPrice] = useState("");
   const handleCheckboxChange = (event, idx) => {
-    const value = event.target.value
-     SetProductBrand(value);
-     const { subcategories } = priductCategories.find((brand) => brand.id == idx -1);
-     setCategoresItem(subcategories)
+    const value = event.target.value;
+    SetProductBrandName(value);
+    const { subcategories } = priductCategories.find(
+      (brand) => brand.id == idx - 1
+    );
+    setCategoresItem(subcategories);
     setCheckedCategory(checkedCategory === idx ? null : idx); // Toggle the selected category
   };
-  console.log(categoresItem)
-    return (
-        <div className='container mx-auto'>
-        <h1 className="text-xl font-semibold">Brand Name</h1>
+  const handleCheckboxItem = (event, idx) => {
+    const item = event.target.value;
+    console.log(idx);
+    setItem(item);
+  };
+  const handleCheckboxPrice = (event, idx) => {
+    const price = event.target.value;
+    setPrice(price)
+    setSelectedPrice(selectedPrice === idx ? null: idx)
+  };
+  console.log(price, "thi is item");
+  return (
+    <div className="container mx-auto">
+        {/* checked box */}
+    <div>
+    <>
+        <h1 className="text-xl font-semibold">Price Range</h1>
+        {priceRange.map((price, idx) => (
+          <div key={idx}>
+            <input
+              key={idx}
+              type="checkbox"
+              id={`checkbox-${idx}`}
+              name="price"
+              value={price}
+              onChange={(event) => handleCheckboxPrice(event, idx)}
+              checked={selectedPrice === idx}
+            />
+            <label htmlFor={`checkbox-${idx}`}>{price}</label>
+          </div>
+        ))}
+      </>
+      <h1 className="text-xl font-semibold">Brand Name</h1>
       {priductCategories.map((category, idx) => (
         <div key={idx}>
           <input
-          key={idx}
+            key={idx}
             type="checkbox"
             id={`checkbox-${idx}`}
             name="category"
@@ -33,24 +65,26 @@ console.log(productBrand)
           />
           <label htmlFor={`checkbox-${idx}`}>{category.brandName}</label>
           {/* Items */}
-          {/* <div className={`${checkedCategory !== idx && 'hidden'} pl-5`}>
-            {category.items.map((item, itemIdx) => (
+          <div className={`${checkedCategory !== idx && "hidden"} pl-5`}>
+            {categoresItem.map((item, itemIdx) => (
               <div key={itemIdx} action="">
                 <input
                   type="checkbox"
                   id={`item-checkbox-${itemIdx}`}
                   name="item"
-                  value={item}
+                  value={item.name}
                   onChange={(event) => handleCheckboxItem(event, itemIdx)}
                 />
-                <label htmlFor={`item-checkbox-${itemIdx}`}>{item}</label>
+                <label htmlFor={`item-checkbox-${itemIdx}`}>{item.name}</label>
               </div>
             ))}
-          </div> */}
+          </div>
         </div>
       ))}
     </div>
-    );
+
+    </div>
+  );
 };
 
 export default Sopping;

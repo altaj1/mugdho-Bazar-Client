446a39dd9bc12f03e7a743afed08a1b19203114c
altaj1 <altaj1019@gmail.com>
2024-08-15 21:23:25 +0600

    implement sorting done

--- src/components/Pages/Sopping/Sopping.jsx
+++ src/components/Pages/Sopping/Sopping.jsx
@@ -7,20 +7,20 @@ import { GrLinkNext } from "react-icons/gr";
 import { GrLinkPrevious } from "react-icons/gr";
 
 const Sopping = () => {
-    const {searchText, setSearchText} = useAuth()
+  const { searchText, setSearchText } = useAuth();
   const axiosCommon = useAxiosCommon();
   const [checkedCategory, setCheckedCategory] = useState(null);
-  const [selectedPrice, setSelectedPrice] = useState(null); 
+  const [selectedPrice, setSelectedPrice] = useState(null);
   const [productBrand, SetProductBrandName] = useState("");
   const [categoresItem, setCategoresItem] = useState([]);
   const [categoryName, setCategoryName] = useState("");
   const [price, setPrice] = useState("");
-  const [currentPage, setCurrentPage] = useState(0); 
+  const [currentPage, setCurrentPage] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(9);
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [productsData, setProductsData] = useState([]);
   const numberOfPages = Math.ceil(count / itemsPerPage);
   const pages = [...Array(numberOfPages).keys()];
-
 
   const handleCheckboxChange = (event, idx) => {
     const value = event.target.value;
@@ -32,7 +32,6 @@ const Sopping = () => {
     setCheckedCategory(checkedCategory === idx ? null : idx); // Toggle the selected category
   };
 
-
   const handleCheckboxItem = (event, idx) => {
     const item = event.target.value;
     console.log(idx);
@@ -41,112 +40,164 @@ const Sopping = () => {
 
   const handleCheckboxPrice = (event, idx) => {
     const price = event.target.value;
-    setPrice(price)
-    setSelectedPrice(selectedPrice === idx ? null: idx)
+    setPrice(price);
+    setSelectedPrice(selectedPrice === idx ? null : idx);
   };
 
   const handlePrevPage = () => {
     if (currentPage > 0) {
-        setCurrentPage(currentPage - 1);
+      setCurrentPage(currentPage - 1);
     }
-}
+  };
 
-const handleNextPage = () => {
+  const handleNextPage = () => {
     if (currentPage < pages.length - 1) {
-        setCurrentPage(currentPage + 1);
+      setCurrentPage(currentPage + 1);
     }
-}
-  const {data: Products = [], isLoading, refetch} = useQuery({
-    queryKey:["productData", productBrand, categoryName, price ],
-    queryFn: async ()=>{
-            const {data} = await axiosCommon.get(`/get-all-product?productBrand=${productBrand}&categoryName=${categoryName}&price=${price}&page=${currentPage}&size=${itemsPerPage}&searchText=${searchText}`)
-            console.log(data)
-            return data
-    }
-  })
-  useEffect( () =>{
-    axiosCommon.get(`/productCount`)
-    
-    .then( res=> setCount(res.data.count))
-        
-}, [currentPage, itemsPerPage])
-console.log(count)
-if (isLoading) {
-    return "Lodiding..."
-}
- 
+  };
+  const {
+    data: products = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: [
+      "productData",
+      productBrand,
+      categoryName,
+      price,
+      currentPage,
+      searchText,
+    ],
+    queryFn: async () => {
+      const { data } = await axiosCommon.get(
+        `/get-all-product?productBrand=${productBrand}&categoryName=${categoryName}&price=${price}&page=${currentPage}&size=${itemsPerPage}&searchText=${searchText}`
+      );
+      console.log(data);
+      return data;
+    },
+  });
+  useEffect(() => {
+    axiosCommon
+      .get(`/productCount`)
+
+      .then((res) => setCount(res.data.count));
+  }, [currentPage, itemsPerPage]);
+
+  if (isLoading) {
+    return "Lodiding...";
+  }
+  const handkeSorting = (value)=>{
+   if ( value == "LowtoHigh" ) {
+    const sortedByLowToHigh = products.sort((a, b) => parseFloat(a.currentPrice) - parseFloat(b.currentPrice));
+    setProductsData(sortedByLowToHigh)
+   }else if
+   ( value == "HightoLow" ) {
+    const sortedByHighToLow = products.sort((a, b) => parseFloat(b.currentPrice) - parseFloat(a.currentPrice));
+    setProductsData(sortedByHighToLow)
+   }
+  }
+  console.log(productsData)
   return (
     <div className="container mx-auto">
- <div>
-         {/* checked box */}
-         <div>
-    <>
-        <h1 className="text-xl font-semibold">Price Range</h1>
-        {priceRange.map((price, idx) => (
-          <div key={idx}>
-            <input
-              key={idx}
-              type="checkbox"
-              id={`checkbox-${idx}`}
-              name="price"
-              value={price}
-              onChange={(event) => handleCheckboxPrice(event, idx)}
-              checked={selectedPrice === idx}
-            />
-            <label htmlFor={`checkbox-${idx}`}>{price}</label>
+      <div>
+        {/* checked box */}
+        <div>
+          {/* sorting */}
+          <div className="dropdown dropdown-hover">
+            <div tabIndex={0} role="button" className="bg-[#8dbe3f] py-2 px-3  rounded-lg  hover:bg-[#5b8021] hover:text-yellow-50 transition-all duration-300 ease-in-out">
+              Hover
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+             <button onClick={()=>handkeSorting("LowtoHigh")} className="bg-[#8dbe3f] py-2  rounded-lg  hover:bg-[#5b8021] hover:text-yellow-50 transition-all duration-300 ease-in-out">Low to High</button>
+             <button onClick={()=>handkeSorting("HightoLow")} className="bg-[#8dbe3f] py-2 mt-1 rounded-lg  hover:bg-[#5b8021] hover:text-yellow-50 transition-all duration-300 ease-in-out">High to Low</button>
+            </ul>
           </div>
-        ))}
-      </>
-      <h1 className="text-xl font-semibold">Brand Name</h1>
-      {priductCategories.map((category, idx) => (
-        <div key={idx}>
-          <input
-            key={idx}
-            type="checkbox"
-            id={`checkbox-${idx}`}
-            name="category"
-            value={category.brandName}
-            onChange={(event) => handleCheckboxChange(event, idx)}
-            checked={checkedCategory === idx}
-          />
-          <label htmlFor={`checkbox-${idx}`}>{category.brandName}</label>
-          {/* Items */}
-          <div className={`${checkedCategory !== idx && "hidden"} pl-5`}>
-            {categoresItem.map((item, itemIdx) => (
-              <div key={itemIdx} action="">
+          <>
+            <h1 className="text-xl font-semibold">Price Range</h1>
+            {priceRange.map((price, idx) => (
+              <div key={idx}>
                 <input
+                  key={idx}
                   type="checkbox"
-                  id={`item-checkbox-${itemIdx}`}
-                  name="item"
-                  value={item.name}
-                  onChange={(event) => handleCheckboxItem(event, itemIdx)}
+                  id={`checkbox-${idx}`}
+                  name="price"
+                  value={price}
+                  onChange={(event) => handleCheckboxPrice(event, idx)}
+                  checked={selectedPrice === idx}
                 />
-                <label htmlFor={`item-checkbox-${itemIdx}`}>{item.name}</label>
+                <label htmlFor={`checkbox-${idx}`}>{price}</label>
               </div>
             ))}
-          </div>
-        </div>
-      ))}
-    </div>
-{/* product */}
-<div></div>
- </div>
-     
- <div className='pagination join flex items-center justify-center p-16'>
-                
-                <button className="flex items-center justify-center gap-1 mr-4" onClick={handlePrevPage}><GrLinkPrevious /> Prev</button>
-               <div className="text-2xl space-x-6">
-               {
-                    pages.map(page => <button
-                        className={`${currentPage == page? 'bg-[#FF6F61] text-yellow-50 w-10 rounded-full':""} `}
-                        onClick={() => setCurrentPage(page)}
-                        key={page}
-                    >{page +1  }</button>)
-                }
-               </div>
-                <button className="flex items-center justify-center gap-1 ml-4" onClick={handleNextPage}>Next <GrLinkNext /></button>
-                
+          </>
+          <h1 className="text-xl font-semibold">Brand Name</h1>
+          {priductCategories.map((category, idx) => (
+            <div key={idx}>
+              <input
+                key={idx}
+                type="checkbox"
+                id={`checkbox-${idx}`}
+                name="category"
+                value={category.brandName}
+                onChange={(event) => handleCheckboxChange(event, idx)}
+                checked={checkedCategory === idx}
+              />
+              <label htmlFor={`checkbox-${idx}`}>{category.brandName}</label>
+              {/* Items */}
+              <div className={`${checkedCategory !== idx && "hidden"} pl-5`}>
+                {categoresItem.map((item, itemIdx) => (
+                  <div key={itemIdx} action="">
+                    <input
+                      type="checkbox"
+                      id={`item-checkbox-${itemIdx}`}
+                      name="item"
+                      value={item.name}
+                      onChange={(event) => handleCheckboxItem(event, itemIdx)}
+                    />
+                    <label htmlFor={`item-checkbox-${itemIdx}`}>
+                      {item.name}
+                    </label>
+                  </div>
+                ))}
+              </div>
             </div>
+          ))}
+        </div>
+        {/* product */}
+        <div></div>
+      </div>
+      {/* pagination */}
+      <div className="pagination join flex items-center justify-center p-16">
+        <button
+          className="flex items-center justify-center gap-1 mr-4"
+          onClick={handlePrevPage}
+        >
+          <GrLinkPrevious /> Prev
+        </button>
+        <div className="text-2xl space-x-6">
+          {pages.map((page) => (
+            <button
+              className={`${
+                currentPage == page
+                  ? "bg-[#FF6F61] text-yellow-50 w-10 rounded-full"
+                  : ""
+              } `}
+              onClick={() => setCurrentPage(page)}
+              key={page}
+            >
+              {page + 1}
+            </button>
+          ))}
+        </div>
+        <button
+          className="flex items-center justify-center gap-1 ml-4"
+          onClick={handleNextPage}
+        >
+          Next <GrLinkNext />
+        </button>
+      </div>
     </div>
   );
 };
